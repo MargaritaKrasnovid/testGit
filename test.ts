8c46320898f672def3188c27a2091f88c3152ab2
MargaritaKrasnovid <40792121+MargaritaKrasnovid@users.noreply.github.com>
2024-01-31 14:46:02 +0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -9,3 +9,11 @@ document.body.insertAdjacentHTML('beforeend', `
     </div>
   </div>
 `)
+
+$('body').append('<div id="weather-alert" class="ntf-alert p-red corner-box-1" style="top: ' + window.innerHeight + 'px">\n' +
+            '<div class="corner-textbox-1" onclick="weatherNotifications.closeAlert()">\n' +
+            '    <div class="ntf-alert-title">' +
+            '       <div>Внимание</div>' +
+            '    </div><br/>' +
+            '    <div class="ntf-close ntf-alert-body-header" title="Закрыть + message + '</div>' +
+            '</div>');
